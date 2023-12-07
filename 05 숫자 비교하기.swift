de507a51b6d7f004ad8e7c9e57459649922e608b
BINDUNGLIFE <144803017+BINDUNGLIFE@users.noreply.github.com>
2023-12-07 11:00:26 +0900

    Create 05 숫자 비교하기.swift

---
+++ 05 숫자 비교하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    if num1 == num2 {
+        return 1
+    } else {
+        return -1
+    }
+    
+}
+
+print(solution(num1: 2, num 3))
+print(solution(num1: 11, num2: 11))
+
